3c78c5eaacdbb471248a76838490164244a0e894
Antyonn <98365466+Antyonn@users.noreply.github.com>
2022-03-07 11:19:41 -0300

    Update and rename Desafio018.py to Seno, Cosseno e Tangente.py

--- Seno, Cosseno e Tangente.py
+++ Seno, Cosseno e Tangente.py
@@ -1,6 +1,6 @@
 from math import sin, cos, tan, radians
 num = int(input('Digite o ângulo: '))
 seno = sin(radians(num))
-coseno = cos(radians(num))
+cosseno = cos(radians(num))
 tangente = tan(radians(num))
-print(f'O seno é {seno:.2f}, o cosseno é {coseno:.2f} e a tangente é {tangente:.2f}')
+print(f'O seno é {seno:.2f}, o cosseno é {cosseno:.2f} e a tangente é {tangente:.2f}')
